3584a0f8bb9ed901f53ea5b8a816dc172b17c014
Jenner Hanni <jeh.wicker@gmail.com>
2013-03-16 08:24:01 -0700

    Changed the way I built the mask.

--- clock-driven-talker.s
+++ clock-driven-talker.s
@@ -164,7 +164,8 @@ STR R0, [R1] 	@ Write word back to ICMR register
 
 LDR R0, =ICMR	@ Load pointer to address of ICMR register
 LDR R1, [R0]	@ Read current value of ICMR
-MOVW R2, #B031  @ Load mask 
+MOVW R2, #0400  @ Load lower sixteen bits of mask 
+MOVT R2, #8000  @ Load upper sixteen bits of mask
 ORR R1, R2	@ Set bits 10 and 31 to unmask IP10 and IP31
 STR R0, [R1] 	@ Write word back to ICMR register
 
