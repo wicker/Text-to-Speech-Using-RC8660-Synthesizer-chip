3461eed12fca5a7411330f4986bf56c8590ffebf
Jenner Hanni <jeh.wicker@gmail.com>
2013-03-16 10:05:46 -0700

    Restoring it to the working version.

--- button-driven-talker.s
+++ button-driven-talker.s
@@ -212,13 +212,15 @@ BTN_SVC:
 	MOV R1, #0x0A	        @ Bit 3 = modem status int, bit 1 = Tx enable
 	STRB R1, [R0]	        @ Write to IER
 
-	B GOBCK		   	@ Go back to wait for the talker interrupt
+	LDMFD SP!, {R0-R1,LR}	@ Restore registers, including return address
+	SUBS PC, LR, #4		@ Return from interrupt to wait loop
 
 @---------------------------------------------------------------------------------@
 @ TLKR_SVC - The interrupt came from the CTS# low or THR empty or other interrupt @
 @---------------------------------------------------------------------------------@
 
 TLKR_SVC:
+	STMFD SP!,{R2-R5}  @ Save additional registers
 
 	LDR R0, =GEDR0		@ Point to GEDR0
 	LDR R1, [R0]		@ Read the current value from GEDR0
@@ -258,7 +260,6 @@ NOCTS:
 @----------------------------------------------------------------@
 
 SEND:
-	STMFD SP!,{R2-R5}  @ Save additional registers
 	LDR R0, =IER	@ Load pointer to IER
 	MOV R1, #0x0A	@ Bit 3 = modem status interrupt, bit 1 = Tx int enable
 	STRB R1, [R0]	@ Write to IER
@@ -285,13 +286,13 @@ SEND:
 	LDR R0, =IER	        @ Pointer to interrupt enable register (IER)
 	MOV R1, #0x00	        @ Bit 3 = modem status int, bit 1 = Tx enable
 	STRB R1, [R0]	        @ Write to IER
-	LDMFD SP!, {R2-R5}	@ Restore additional registers
 
 @------------------------------------@
 @ GOBCK - Restore from the interrupt @
 @------------------------------------@
 
 GOBCK:
+	LDMFD SP!, {R2-R5}	@ Restore additional registers
 	LDMFD SP!, {R0-R1,LR}	@ Restore original registers, including return address
 	SUBS PC, LR, #4		@ Return from interrupt (to wait loop)
 
