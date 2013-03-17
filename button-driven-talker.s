590fc4e492cc8a203fb56a843000bb518918f89f
Jenner Hanni <jeh.wicker@gmail.com>
2013-03-17 11:39:07 -0700

    Restore all registers at the same time in SEND.

--- button-driven-talker.s
+++ button-driven-talker.s
@@ -285,9 +285,8 @@ SEND:
 	LDR R0, =IER	        @ Pointer to interrupt enable register (IER)
 	MOV R1, #0x00	        @ Bit 3 = modem status int, bit 1 = Tx enable
 	STRB R1, [R0]	        @ Write to IER
-	LDMFD SP!, {R2-R5}	@ Restore additional registers
-	LDMFD SP!, {R0-R1,LR}	@ Restore original registers, including return address
-	SUBS PC, LR, #8		@ Return from interrupt (to wait loop)
+	LDMFD SP!, {R0-R5, LR}	@ Restore all registers
+	SUBS PC, LR, #4		@ Return from interrupt (to wait loop)
 
 @------------------------------------@
 @ GOBCK - Restore from the interrupt @
